475a8d81ea7cf9e74b3b5ec4a23d6addb8a6b351
amanunur <46161275+amanunur@users.noreply.github.com>
2020-07-14 11:46:14 +0700

    Hari ke 8 bagian 2

--- src/component/Atom/Input/index.js
+++ src/component/Atom/Input/index.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {TextInput} from 'react-native-gesture-handler';
-import {colors} from '../../../utils/colors';
-import { fonts } from '../../../utils';
+import {fonts, colors} from '../../../utils';
 
-export default function index({title}) {
+export default function index({title, value, onChangeText, secureTextEntry}) {
+  const [border, setBorder] = useState(colors.border);
+  const onFocusForm = () => {
+    setBorder(colors.tertiary);
+  };
+  const onBlurFrom = () => {
+    setBorder(colors.border);
+  };
+
   return (
     <View>
       <Text style={styles.text}>{title}</Text>
-      <TextInput style={styles.input} />
+      <TextInput
+        onFocus={onFocusForm}
+        onBlur={onBlurFrom}
+        style={styles.input(border)}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
+  input: (border) => ({
     borderWidth: 1,
     borderRadius: 10,
-    borderColor: colors.border,
+    borderColor: border,
     padding: 12,
     marginTop: 6,
-  },
+  }),
+
   text: {
     fontFamily: fonts.primary.normal,
     color: colors.text.secondary,
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, ScrollView} from 'react-native';
 import {Button, Gap, Header, Input} from '../../component';
 import {colors} from '../../utils/colors';
 
 export default function Register({navigation}) {
+  const [fullName, setFullName] = useState('');
+  const [profession, setProfession] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onContinue = () => {
+    console.log(fullName, profession, email, password);
+  };
+
   return (
     <View style={styles.container}>
       <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
       <View style={styles.content}>
-        <Input title="Full Name" />
-        <Gap height={24} />
-        <Input title="Pekerjaan" />
-        <Gap height={24} />
-        <Input title="Email Address" />
-        <Gap height={24} />
-        <Input title="Password" />
-        <Gap height={40} />
-        <Button
-          title="Continue"
-          onPress={() => navigation.navigate('UploadPhoto')}
-        />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Input
+            title="Full Name"
+            value={fullName}
+            onChangeText={(value) => setFullName(value)}
+          />
+          <Gap height={24} />
+          <Input
+            title="Pekerjaan"
+            value={profession}
+            onChangeText={(value) => setProfession(value)}
+          />
+          <Gap height={24} />
+          <Input
+            title="Email Address"
+            value={email}
+            onChangeText={(value) => setEmail(value)}
+          />
+          <Gap height={24} />
+          <Input
+            title="Password"
+            value={password}
+            onChangeText={(value) => setPassword(value)}
+            secureTextEntry
+          />
+          <Gap height={40} />
+          <Button title="Continue" onPress={onContinue} />
+        </ScrollView>
       </View>
     </View>
   );
